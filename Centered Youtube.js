117225e850f4b59a909f08ca3205db6a514c5740
Konstantin Budnikov <angryrussian26@gmail.com>
2012-12-24 21:37:25 -0800

    Results Pages
    Only replacing the classes that needs replacing using the replace
    fuction. It is better this way because if Youtube adds more classes
    inside the string, then all my changes would of been gone. This way if
    they add on to the string, it will still work.

--- Centered Youtube.js
+++ Centered Youtube.js
@@ -1,19 +1,27 @@
 // ==UserScript==
 // @name         Centered Youtube
 // @namespace    www.technikov.com
-// @description  This script aligns the new 2012 Youtube layout in the center of your browser. 
+// @description  The script aligns the new 2012 Youtube layout in the center of your browser. 
 // @include      *.youtube.com/*
+// @match        http://*.youtube.com/*
+// @match        https://*.youtube.com/*
+// @include      https://*.youtube.com/*
+// @include      http://*.youtube.com/*
 // @version      1.0
 // @copyright    2012+, Konstantin Budnikov and Ryan Laguna 
 // @license      (CC) Attribution Share Alike; http://creativecommons.org/licenses/by-sa/3.0/
 // ==/UserScript==
+
 // This uses JacaScript to replace the site-left-aligned CSS with site-center-aligned.
 document.body.className = document.body.className.replace('site-left-aligned', 'site-center-aligned');
+
 // This uses path to find particular class or ids and replace them with new CSS. 
+// Only replacing the classes that need to be replaced with the replace function.
 if (location.pathname === "/results"){
-    document.body.className="ltr  gecko gecko-19      site-center-aligned     exp-watch7-comment-ui hitchhiker-enabled      guide-enabled       guide-expanded "; 
+    document.body.className = document.body.className.replace('site-left-aligned', 'site-center-aligned');
+	document.body.className = document.body.className.replace('exp-new-site-width', '');
 }
-if else (location.pathname === "/watch")
+if (location.pathname === "/watch")
 {
     document.body.className="ltr gecko gecko-19 site-center-aligned exp-new-site-width exp-watch7-comment-ui hitchhiker-enabled guide-enabled guide-collapsed sidebar-expanded ";
     document.getElementById('guide-container').style.left="-70px";
