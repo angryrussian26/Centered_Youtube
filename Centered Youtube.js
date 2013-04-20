14879af5180796fad0e9b4489a7df465903d13be
angryrussian26 <angryrussian26@gmail.com>
2013-04-20 15:55:55 -0700

    Ryan's 4/20/13 new layout updates

--- Centered Youtube.js
+++ Centered Youtube.js
@@ -1,34 +1,28 @@
 // ==UserScript==
 // @name         Centered Youtube
 // @namespace    http://www.technikov.com/
-// @description  The script aligns the new 2012 Youtube layout in the center of your browser. 
+// @description  The script aligns the new 2013 YouTube layout in the center of your browser. 
 // @include      *.youtube.com/*
 // @match        http://*.youtube.com/*
 // @match        https://*.youtube.com/*
 // @include      https://*.youtube.com/*
 // @include      http://*.youtube.com/*
-// @version      1.2
-// @copyright    2012+, Konstantin Budnikov and Ryan Laguna 
+// @version      1.5
+// @copyright    2013, Konstantin Budnikov and Ryan Laguna 
 // @license      (CC) Attribution Share Alike; http://creativecommons.org/licenses/by-sa/3.0/
-// @icon 		 http://img515.imageshack.us/img515/9339/youtubeuw.png
+// @icon         http://img515.imageshack.us/img515/9339/youtubeuw.png
+// @grant        none
 // ==/UserScript==
 
-// This uses JacaScript to replace the site-left-aligned CSS with site-center-aligned.
-document.body.className = document.body.className.replace('site-left-aligned', 'site-center-aligned');
 
-// Fixes YouTube masthead and footer
-document.getElementById('yt-masthead').style.maxWidth = '1003px';
-document.getElementById('footer-hh').style.maxWidth = '1003px';
+// Applies backgrounds.
+document.body.style.background = '#f1f1f1';
+document.getElementById('body-container').style.background = 'white';
 
-// This uses path to find particular class or ids and replace them with new CSS. 
-// Only replacing the classes that need to be replaced with the replace function.
-if (location.pathname === "/results"){
-	document.body.className = document.body.className.replace('exp-new-site-width', '');
-	document.getElementById('yt-masthead').style.maxWidth = '971px';
-	document.getElementById('footer-hh').style.maxWidth = '971px';
-}
-if (location.pathname === "/watch")
-{
-	document.getElementById('yt-masthead').style.maxWidth = '1250px';
-	document.getElementById('footer-hh').style.maxWidth = '1250px';
-}
+// Sets width and centers page.
+document.getElementById('body-container').style.maxWidth = '1304px';
+document.getElementById('body-container').style.marginLeft = 'auto';
+document.getElementById('body-container').style.marginRight = 'auto';
+document.getElementById('footer-container').style.cssText = 'max-width: 1304px !important';
+document.getElementById('footer-container').style.marginLeft = 'auto';
+document.getElementById('footer-container').style.marginRight = 'auto';
